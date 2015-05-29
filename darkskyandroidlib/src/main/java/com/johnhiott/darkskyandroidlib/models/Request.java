e4d73b3029c5cd9e741dccc0c5fd380407ecf631
John Hiott <john.hiott@passportparking.com>
2015-05-29 10:30:15 -0400

    add fields to request, and functions for query params

--- darkskyandroidlib/src/main/java/com/johnhiott/darkskyandroidlib/models/Request.java
+++ darkskyandroidlib/src/main/java/com/johnhiott/darkskyandroidlib/models/Request.java
@@ -2,15 +2,42 @@ package com.johnhiott.darkskyandroidlib.models;
 
 import android.util.Log;
 
+import com.google.common.base.Joiner;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Request {
 
-    public static final String UNITS = "units";
-    public static final String SI_UNITS = "si";
-    public static final String US_UNITS = "us";
+    private static final String UNITS_KEY = "units";
+    public static final String UNITS_SI = "si";
+    public static final String UNITS_US = "us";
+    public static final String UNITS_CA = "ca";
+    public static final String UNITS_UK = "uk";
+    public static final String UNITS_AUTO = "auto";
+
+    private static final String LANGUAGE_KEY = "lang";
+    public static final String LANG_ES = "es";
+    public static final String LANG_PIG_LATIN = "x-pig-latin";
+
+    private static final String EXCLUDE_KEY = "exclude";
+    /** Exclude the currently block */
+    public static final String EXCLUDE_CURRENTLY = "currently";
+    /** Exclude the minutely block */
+    public static final String EXCLUDE_MINUTELY = "minutely";
+    public static final String EXCLUDE_HOURLY = "hourly";
+    public static final String EXCLUDE_DAILY = "daily";
+    public static final String EXCLUDE_ALERTS = "alerts";
+    public static final String EXCLUDE_FLAGS = "flags";
 
     private String mLat;
     private String mLng;
     private String mTime;
+    private String mUnits;
+    private String mLanguage;
+    private List<String> mExcludeBlock = new ArrayList<String>();
 
     public String getLat() {
         return mLat;
@@ -40,10 +67,48 @@ public class Request {
         return mTime != null && !mTime.equals("");
     }
 
+    public String getUnits() {
+        return mUnits;
+    }
+
+    public void setUnits(String units) {
+        mUnits = units;
+    }
+
+    public String getLanguage() {
+        return mLanguage;
+    }
+
+    public void setLanguage(String language) {
+        mLanguage = language;
+    }
+
+    public void addToExcludeBlock(String exclude) {
+        mExcludeBlock.add(exclude);
+    }
+
+    public void removeFromExcludeBlock(String exclude) {
+        int index = mExcludeBlock.indexOf(exclude);
+        if (index != -1) mExcludeBlock.remove(index);
+    }
+
+    public Map<String, String> getQueryParams() {
+        Map<String, String> query = new HashMap<String, String>();
+        query.put(UNITS_KEY, mUnits);
+        query.put(LANGUAGE_KEY, mLanguage);
+        query.put(EXCLUDE_KEY, getExcludeBlock());
+        return query;
+    }
+
+    private String getExcludeBlock() {
+        return mExcludeBlock.size() > 0 ? Joiner.on(",").join(mExcludeBlock) : null;
+    }
+
+    /** @hide */
     @Override
     public String toString() {
         String params = mLat + "," + mLng;
-        Log.d("JOJO", useTime().toString());
         return  useTime() ?  params + "," + mTime : params;
     }
+
 }
