65f3517cf05544c7078bef89a47c8543778dc1cf
John Hiott <john.hiott@passportparking.com>
2015-05-29 10:30:30 -0400

    add data point model

---
+++ darkskyandroidlib/src/main/java/com/johnhiott/darkskyandroidlib/models/DataPoint.java
@@ -0,0 +1,160 @@
+package com.johnhiott.darkskyandroidlib.models;
+
+public class DataPoint {
+
+    private long time;
+    private String summary;
+    private String icon;
+    private String sunriseTime;
+    private String moonPhase;
+    private String nearestStormDistance;
+    private String nearestStormBearing;
+    private String precipIntensity;
+    private String precipIntensityMax;
+    private String precipIntensityMaxTime;
+    private String precipProbability;
+    private String precipType;
+    private String precipAccumulation;
+    private double temperature;
+    private double temperatureMin;
+    private double temperatureMinTime;
+    private double temperatureMax;
+    private double temperatureMaxTime;
+    private double apparentTemperature;
+    private double apparentTemperatureMin;
+    private double apparentTemperatureMinTime;
+    private double apparentTemperatureMax;
+    private double apparentTemperatureMaxTime;
+    private String dewPoint;
+    private String windSpeed;
+    private String windBearing;
+    private String cloudClover;
+    private String humidity;
+    private String pressure;
+    private String visibility;
+    private String ozone;
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getSunriseTime() {
+        return sunriseTime;
+    }
+
+    public String getMoonPhase() {
+        return moonPhase;
+    }
+
+    public String getNearestStormDistance() {
+        return nearestStormDistance;
+    }
+
+    public String getNearestStormBearing() {
+        return nearestStormBearing;
+    }
+
+    public String getPrecipIntensity() {
+        return precipIntensity;
+    }
+
+    public String getPrecipIntensityMax() {
+        return precipIntensityMax;
+    }
+
+    public String getPrecipIntensityMaxTime() {
+        return precipIntensityMaxTime;
+    }
+
+    public String getPrecipProbability() {
+        return precipProbability;
+    }
+
+    public String getPrecipType() {
+        return precipType;
+    }
+
+    public String getPrecipAccumulation() {
+        return precipAccumulation;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public double getTemperatureMin() {
+        return temperatureMin;
+    }
+
+    public double getTemperatureMinTime() {
+        return temperatureMinTime;
+    }
+
+    public double getTemperatureMax() {
+        return temperatureMax;
+    }
+
+    public double getTemperatureMaxTime() {
+        return temperatureMaxTime;
+    }
+
+    public double getApparentTemperature() {
+        return apparentTemperature;
+    }
+
+    public double getApparentTemperatureMin() {
+        return apparentTemperatureMin;
+    }
+
+    public double getApparentTemperatureMinTime() {
+        return apparentTemperatureMinTime;
+    }
+
+    public double getApparentTemperatureMax() {
+        return apparentTemperatureMax;
+    }
+
+    public double getApparentTemperatureMaxTime() {
+        return apparentTemperatureMaxTime;
+    }
+
+    public String getDewPoint() {
+        return dewPoint;
+    }
+
+    public String getWindSpeed() {
+        return windSpeed;
+    }
+
+    public String getWindBearing() {
+        return windBearing;
+    }
+
+    public String getCloudClover() {
+        return cloudClover;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getPressure() {
+        return pressure;
+    }
+
+    public String getVisibility() {
+        return visibility;
+    }
+
+    public String getOzone() {
+        return ozone;
+    }
+}
