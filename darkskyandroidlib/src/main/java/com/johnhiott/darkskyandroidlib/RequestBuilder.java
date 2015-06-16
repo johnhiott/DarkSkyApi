f497007a5fccc26674027b6231dfe360e34e1b32
John Hiott <jhiott@oolcay.com>
2015-06-16 09:35:49 -0400

    added rxjava method to request helper

--- darkskyandroidlib/src/main/java/com/johnhiott/darkskyandroidlib/RequestBuilder.java
+++ darkskyandroidlib/src/main/java/com/johnhiott/darkskyandroidlib/RequestBuilder.java
@@ -4,6 +4,7 @@ import com.johnhiott.darkskyandroidlib.models.Request;
 import com.johnhiott.darkskyandroidlib.models.WeatherResponse;
 
 import retrofit.Callback;
+import rx.Observable;
 
 public class RequestBuilder {
 
@@ -15,5 +16,9 @@ public class RequestBuilder {
 
     public void getWeather(Request request, Callback<WeatherResponse> callback) {
         mWeather.getWeather(request, request.getQueryParams(), callback);
+    }
+
+    public Observable<WeatherResponse> getWeather(Request request) {
+        return mWeather.getWeather(request, request.getQueryParams());
     }
 }
